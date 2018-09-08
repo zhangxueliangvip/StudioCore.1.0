5293f0f3b77449d354a8a4cfc4d3d18114fe8a84
Zack <40447057+zhangxueliangvip@users.noreply.github.com>
2018-09-08 10:46:34 +0800

    Manager Redis 缓存

---
+++ Manager/RedisManager/RedisManagerCore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisManager
+{
+  public partial class RedisManagerCore
+    {
+        #region 单例模式
+        private static readonly RedisManagerCore instance = new RedisManagerCore();
+        private RedisManagerCore() { }
+        public static RedisManagerCore GetInstance => instance;
+        #endregion
+    }
+}
